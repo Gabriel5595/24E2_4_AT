012aac010d5f2bee23003418a7b86b3ffec9aa6d
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-06-24 19:56:48 -0300

    Atualização da função save_to_json

--- components/save_json_file.py
+++ components/save_json_file.py
@@ -1,6 +1,14 @@
-from components.read_db import read_bd
-from components.search_car_plate import search_car_plate
+import json
 
 def save_json_file(json_info, file_path):
-    with open(file_path, 'w') as arquivo:
-        arquivo.write(json_info)
+    try:
+        with open(file_path, 'w') as f:
+            json.dump(json_info, f, indent=4)
+        print(f"Arquivo '{file_path}' salvo com sucesso.")
+        return True
+    except FileNotFoundError:
+        print(f"Erro: Caminho '{file_path}' não encontrado.")
+        return False
+    except Exception as e:
+        print(f"Erro ao salvar arquivo '{file_path}': {str(e)}")
+        return False
--- tests/save_json_file.py
+++ tests/save_json_file.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import os
 # Adiciona o diretório raiz do projeto ao sys.path
@@ -7,8 +8,17 @@ from components.read_db import read_bd
 from components.search_car_plate import search_car_plate
 
 def save_json_file(json_info, file_path):
-    with open(file_path, 'w') as arquivo:
-        arquivo.write(json_info)
+    try:
+        with open(file_path, 'w') as f:
+            json.dump(json_info, f, indent=4)
+        print(f"Arquivo '{file_path}' salvo com sucesso.")
+        return True
+    except FileNotFoundError:
+        print(f"Erro: Caminho '{file_path}' não encontrado.")
+        return False
+    except Exception as e:
+        print(f"Erro ao salvar arquivo '{file_path}': {str(e)}")
+        return False
 
 def main():
     file_path_bd = "C:/Users/gribe/OneDrive/Documentos/Codes/INFNET/2024.1/Python para Dados/TPs/AT/resources/cars_db.csv"
