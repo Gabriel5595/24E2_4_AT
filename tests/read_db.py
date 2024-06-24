4f37c077d8b0e3cd7ddeac8c5e4c49c5c5a5bb67
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-06-24 17:34:13 -0300

    Finalização da função read_db

---
+++ tests/read_db.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sys
+import os
+# Adiciona o diretório raiz do projeto ao sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+def read_bd(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+def main():
+    file_path = "C:/Users/gribe/OneDrive/Documentos/Codes/INFNET/2024.1/Python para Dados/TPs/AT/resources/cars_db.csv"
+    print(read_bd(file_path))
+
+main()
